09ad6b58076b3d6826f3588ecdb1776080a8f44a
linuxmobile <bdiez19@gmail.com>
2023-07-28 16:45:51 -0300

    Feat: added marquee

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,23 @@ module.exports = {
       fontFamily: {
         'mona-sans': ['"Mona Sans"'],
       },
+      fontSize: {
+        'xs': ['0.65rem', { lineHeight: '0.9rem' }], 
+      },
+			animation: {
+				marquee: 'marquee 25s linear infinite',
+				marquee2: 'marquee2 25s linear infinite',
+			},
+			keyframes: {
+				marquee: {
+					'0%': { transform: 'translateX(0%)' },
+					'100%': { transform: 'translateX(-100%)' },
+				},
+				marquee2: {
+					'0%': { transform: 'translateX(100%)' },
+					'100%': { transform: 'translateX(0%)' },
+				},
+			},
     },
 	},
 	plugins: [],
